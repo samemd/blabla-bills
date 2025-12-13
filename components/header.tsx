0c5b595e4d0bb6b269468a94e3ff423ae6b81dcd
Samuel Emde <samuel.emde@youengineering.ch>
2025-12-13 17:12:16 +0100

    feat: make logo clickable

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,19 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="mx-auto flex max-w-5xl items-center justify-between gap-6 px-6 py-6">
       <div className="flex items-center gap-3">
-        <div className="flex h-9 font-bold w-9 items-center justify-center rounded-md bg-gradient-to-br from-blue-600 to-fuchsia-500 text-white shadow">
+        <Link
+          href="/"
+          className="flex h-9 font-bold w-9 items-center justify-center rounded-md bg-gradient-to-br from-blue-600 to-fuchsia-500 text-white shadow"
+        >
           BB
-        </div>
-        <div className="text-xl font-semibold text-foreground">BlablaBills</div>
+        </Link>
+        <Link href="/" className="text-xl font-semibold text-foreground">
+          BlablaBills
+        </Link>
       </div>
       <div className="flex items-center gap-4">
         <div className="hidden text-sm  dark:text-slate-400 sm:block">
